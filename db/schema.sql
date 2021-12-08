9b64952682ddedef3d18943a0f14e15e643773d0
danny p <danny@dannys-MacBook-Pro.local>
2021-12-08 13:41:56 -0500

    seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,17 @@
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
+
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
+    title VARCHAR(30),
     salary DECIMAL,
     department_id INTEGER,
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
@@ -21,5 +23,5 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
-    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,12 +5,12 @@ VALUES
 ('Finance'),
 ('Legal');
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES 
-('Software Engineer', 120,000.00, 1),
-('Lead Engineer', 150,000.00, 1),
-('Salesperson', 80,000.00, 2),
-('Account Manager', 160,000.00, 3),
-('Accountant', 125,000.00, 3),
-('Lawyer', 190,000.00, 4),
-('Legal Team Lead', 250,000.00, 4);
+('Software Engineer', 120000.00, 1),
+('Lead Engineer', 150000.00, 1),
+('Salesperson', 80000.00, 2),
+('Account Manager', 160000.00, 3),
+('Accountant', 125000.00, 3),
+('Lawyer', 190000.00, 4),
+('Legal Team Lead', 250000.00, 4);
