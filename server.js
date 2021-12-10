333a8ac5fd3180d58819de1af32c1e99c24b43cf
danny p <danny@dannys-MacBook-Pro.local>
2021-12-10 00:01:21 -0500

    inquirer in progress

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const db = require('./db/connection');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
 
 // routes
 const apiRoutes = require('./routes/apiRoutes');
+
 
 // express middleware
 app.use(express.urlencoded({extended: false}));
@@ -12,6 +15,62 @@ app.use(express.json());
 
 // url defined
 app.use('/api', apiRoutes);
+
+// comman line questions
+
+const promptUser = () => {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'How would you like to proceed?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role'
+            ]
+        }
+    ])
+    .then((answers) => {
+
+        const { choices } = answers;
+
+        if (choices === "View all departments") {
+            getDepartments();
+        }
+    })
+        
+}
+
+
+    getDepartments = () => {
+        console.log("Showing all departments");
+
+       
+            const sql = `SELECT * FROM departments`;
+            db.query(sql, (err, rows) => {
+                if (err) throw err;
+                console.table(rows);
+                promptUser();
+            });
+
+        };    
+        
+    
+            
+    
+  
+
+
+
+
+    promptUser();
+
+
 
 
 app.use((req, res) => {
